fce746eeef4e720217cf7f72899c1a5f9e583ffa
kato-yutaro <utarow49@gmail.com>
2023-06-04 19:50:05 +0900

    抽象構文木を導入

--- 9cc.c
+++ 9cc.c
@@ -110,6 +110,82 @@ Token *tokenize() {
     return head.next;
 }
 
+//抽象構文木のノードの種類
+typedef enum {
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+//抽象構文木のノード
+struct Node {
+    NodeKind kind; 
+    Node *lhs; //左子ノード
+    Node *rhs; //右子ノード
+    int val;
+};
+
+Node* new_node(NodeKind kind, Node *lhs, Node* rhs) {
+    Node* node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node* new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node* expr();
+Node* primary();
+
+Node* expr() {
+    Node *node = primary();
+    for(;;) {
+        if(consume('+'))
+            node = new_node(ND_ADD, node, primary());
+        else if (consume('-'))
+            node = new_node(ND_SUB, node, primary());
+        else
+            return node;
+    }
+}
+
+Node *primary() {
+    return new_node_num(expect_number());
+}
+
+void gen(Node *node) {
+    if(node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->kind) {
+        case ND_ADD:
+            printf("add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("  sub rax, rdi\n");
+    }
+
+    printf("  push rax\n");
+}
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         fprintf(stderr, "引数の個数が正しくありません");
@@ -118,23 +194,26 @@ int main(int argc, char** argv) {
 
     user_input = argv[1];
     token = tokenize();
+    Node* node = expr();
 
-    char *p = argv[1];
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", expect_number());
+    // printf("  mov rax, %d\n", expect_number());
 
-    while(!at_eof()) {
-        if(consume('+')) {
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
+    // while(!at_eof()) {
+    //     if(consume('+')) {
+    //         printf("  add rax, %d\n", expect_number());
+    //         continue;
+    //     }
 
-        expect('-');
-        printf("  sub rax, %d\n", expect_number());
-    }
+    //     expect('-');
+    //     printf("  sub rax, %d\n", expect_number());
+    //}
 
+    gen(node);
+
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }
